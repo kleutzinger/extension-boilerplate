2a04547d3fef412edaf71bebb1210c89b50b732f
kleutzinger <thedookmaster@gmail.com>
2020-06-12 13:22:22 -0700

    reformatting / working

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,7 +1,7 @@
-import ext from "./utils/ext";
-import storage from "./utils/storage";
+import ext from './utils/ext';
+import storage from './utils/storage';
 
-var colorSelectors = document.querySelectorAll(".js-radio");
+var colorSelectors = document.querySelectorAll('.js-radio');
 
 var setColor = (color) => {
   document.body.style.backgroundColor = color;
@@ -10,21 +10,21 @@ var setColor = (color) => {
 storage.get('color', function(resp) {
   var color = resp.color;
   var option;
-  if(color) {
+  if (color) {
     option = document.querySelector(`.js-radio.${color}`);
     setColor(color);
   } else {
-    option = colorSelectors[0]
+    option = colorSelectors[0];
   }
 
-  option.setAttribute("checked", "checked");
+  option.setAttribute('checked', 'checked');
 });
 
 colorSelectors.forEach(function(el) {
-  el.addEventListener("click", function(e) {
+  el.addEventListener('click', function(e) {
     var value = this.value;
     storage.set({ color: value }, function() {
       setColor(value);
     });
-  })
-})
+  });
+});
